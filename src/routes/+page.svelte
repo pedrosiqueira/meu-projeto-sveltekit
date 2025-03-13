13ac1e5b2621ca7a744cdd8bbbcaebde11cd6ce5
PedroSiqueira <pedro.siqueira@ifms.edu.br>
2025-03-13 12:03:58 -0400

    página inicial alterada

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<p>Bem vindo ao meu site SvelteKit! Saiba mais <a href="/sobre">aqui</a>.</p>
+<h1>Bem-vindo ao meu site!</h1>
+<p>Sinta-se à vontade. Saiba mais <a href="/sobre">aqui</a>.</p>
 <p>Entre em contato <a href="/contato">aqui</a>.</p>
